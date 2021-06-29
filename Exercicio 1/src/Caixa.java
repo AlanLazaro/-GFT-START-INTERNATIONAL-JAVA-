0179951ec77868775af46c68a400cb350a75d559
Alan Lazaro <alantsl10@gmail.com>
2021-06-29 16:01:59 -0300

    1

--- Exercicio 1/src/Caixa.java
+++ Exercicio 1/src/Caixa.java
@@ -1,6 +1,21 @@
 public class Caixa {
     public double calculaValorFinal(Produto p, int q){
+        double valor=0.0;
+        if(p.getTipo()==1){
+            valor = p.getValor() * q;
+            valor -= valor*0.1;
+        }
 
+        if(p.getTipo()==2){
+            valor = p.getValor() * q;
+            valor -= valor*0.2;
+        }
+
+        if(p.getTipo()==3){
+            valor = p.getValor() * q;
+            if(q > 5)
+                valor -= valor*0.1;
+        }
         return valor;
     }
 }
